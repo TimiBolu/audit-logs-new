3036a5ffe2fc06b42d550ca1abfff3f37f298381
Oluwatimilehin Adesina <adesinaim@gmail.com>
2023-08-14 20:07:35 +0100

    remove typeds

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from "./getUserIpAddress";
 export * from "./aws/getLogs";
 export * from "./aws/logToS3";
 export * from "./config/s3.config";
-export * from "./typings/config.type";
 
 // import { getUserIpAddress } from "./getUserIpAddress";  
 // import { logUserAction } from "./aws/logToS3";
